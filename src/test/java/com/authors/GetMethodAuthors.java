cd7a036f223030a23a84d446f0da491a32ffe2c7
Chaitanya4803 <pchaitanya7799@gmail.com>
2025-02-07 17:26:37 +0530

    Swagger first Commit

---
+++ src/test/java/com/authors/GetMethodAuthors.java
@@ -0,0 +1,34 @@
+package com.authors;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetMethodAuthors {
+	@Test
+	public void testGetAllAuthors() {
+		given()
+		.when()
+		.get("https://fakerestapi.azurewebsites.net/api/v1/Authors")
+		.then()
+		.statusCode(200)
+		.body("size()", greaterThan(0))
+		.log().all();
+	}
+	@Test
+	public void testGetAuthorsByBookId() {
+		given()
+		.when() 
+		.get("https://fakerestapi.azurewebsites.net/api/v1/Authors/authors/books/1")
+		.then()
+		.statusCode(200)
+		.body("size()", greaterThan(0));
+	}
+	@Test
+	public void testGetAuthorById() {
+		given().when().get("https://fakerestapi.azurewebsites.net/api/v1/Authors/1")
+		.then().statusCode(200).body("id", equalTo(1));
+	}
+
+}
