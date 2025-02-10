35dd5b7c17a482e015716a850052954a60163ffa
SujataSonawane4001 <sujata62sonawane@gmail.com>
2025-02-10 12:57:31 +0530

    Activities GET Methods completed

---
+++ src/test/java/com/Activities/PostMethodActivities.java
@@ -0,0 +1,24 @@
+package com.Activities;
+
+public class PostMethodActivities {
+	
+//	public void VerifyCreateActivity() {
+//		String requestBody="{\r\n"
+//				+ "  \"id\": 0,\r\n"
+//				+ "  \"title\": \"string\",\r\n"
+//				+ "  \"dueDate\": \"2025-02-09T10:18:15.718Z\",\r\n"
+//				+ "  \"completed\": true\r\n"
+//				+ "}";
+//		 given()
+//		.header("Content-Type","application/json")
+//		.body(requestBody)
+//		.when()
+//		.post("/api/v1/Activities")
+//		.then()
+//		.statusCode(201)
+//		.body()
+//		
+//
+//	}
+
+}
