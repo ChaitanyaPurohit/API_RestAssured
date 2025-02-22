56868c361159d130558c1fdddf4b476d26189026
Chaitanya4803 <pchaitanya7799@gmail.com>
2025-02-22 13:31:55 +0530

    Changes on 22/02

---
+++ src/test/java/com/authorsrom/DeletMethodAuthorsUsingRom.java
@@ -0,0 +1,7 @@
+package com.authorsrom;
+
+public class DeletMethodAuthorsUsingRom {
+	public static void main(String[] args) {
+		System.out.println("chaitanya");
+	}
+}
--- src/test/java/com/authorsrom/GetMethodAuthorsUsingRom.java
+++ src/test/java/com/authorsrom/GetMethodAuthorsUsingRom.java
@@ -2,7 +2,7 @@ package com.authorsrom;
 
 import org.testng.annotations.Test;
 
-import com.authors.requestspecs.RequestSpecsForGet;
+import com.authors.requestspecs.AuthorRequests;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -10,27 +10,27 @@ public class GetMethodAuthorsUsingRom {
 
 	@Test
 	public void testGetAllAuthors() {
-		RequestSpecsForGet.getAllAuthors().then().statusCode(200).body("size()", greaterThan(0)).log().all();
+		AuthorRequests.getAllAuthors().then().statusCode(200).body("size()", greaterThan(0)).log().all();
 	}
 
 	@Test
 	public void testGetAuthorsByBookId() {
-		RequestSpecsForGet.getAuthorsByBookId(1).then().log().all().statusCode(200).body("size()", greaterThan(0));
+		AuthorRequests.getAuthorsByBookId(1).then().log().all().statusCode(200).body("size()", greaterThan(0));
 	}
 
 	@Test
 	public void testGetAuthorById() {
-		RequestSpecsForGet.getAuthorById(1).then().statusCode(200).body("id", equalTo(1));
+		AuthorRequests.getAuthorById(2).then().statusCode(200).body("id", equalTo(2));
 	}
 
 	@Test
 	public void testGetNonExistingAuthorById() {
-		RequestSpecsForGet.getNonExistingAuthorById().then().statusCode(404);
+		AuthorRequests.getNonExistingAuthorById().then().statusCode(404);
 	}
 
 	@Test
 	public void testGetAuthorsWithInvalidIdFormat() {
-		RequestSpecsForGet.getAuthorsWithInvalidIdFormat().then().statusCode(400);
+		AuthorRequests.getAuthorsWithInvalidIdFormat().then().statusCode(400);
 	}
 
 }
